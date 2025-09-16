a98b32f012d63f68f95592370fa958afe001f1f5
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-09-16 02:35:29 -0700

    Animate between focused rows

--- components/Inbox/Inbox.tsx
+++ components/Inbox/Inbox.tsx
@@ -7,10 +7,10 @@ import {
 } from "@radix-ui/react-roving-focus"
 
 // Hooks
-import { useState } from "react"
+import { useCallback, useLayoutEffect, useRef, useState } from "react"
 
 // Types
-import type { KeyboardEvent, MouseEvent } from "react"
+import type { CSSProperties, KeyboardEvent, MouseEvent } from "react"
 import type { ClientFriendlyTransaction } from "@/functions/db/transactions"
 
 import styles from "./Inbox.module.css"
@@ -47,10 +47,40 @@ export function Inbox({
   }
 
   const [activeId, setActiveId] = useState(transactions[0].id)
+  const olRef = useRef<HTMLOListElement>(null)
+  const liRefs = useRef<Record<string, HTMLElement>>({})
+  const [highlightStyles, setHighlightStyles] = useState<CSSProperties>({
+    transform: "translateY(0px)",
+    height: "38px" // rough estimation of single-line item height
+  })
+
+  const updateHighlight = useCallback((id: string) => {
+    const ol = olRef.current
+    const li = liRefs.current[id]
+    if (!ol || !li) return
+
+    const olRect = ol.getBoundingClientRect()
+    const liRect = li.getBoundingClientRect()
+
+    setHighlightStyles({
+      transform: `translateY(${liRect.top - olRect.top}px)`,
+      height: `${liRect.height}px`
+    })
+  }, [])
+
+  useLayoutEffect(() => {
+    updateHighlight(activeId)
+  }, [activeId, updateHighlight])
 
   return (
     <RovingFocusGroup orientation="vertical">
-      <ol className={styles.list} role="listbox" aria-label="Transaction Inbox">
+      <ol
+        className={styles.list}
+        role="listbox"
+        aria-label="Transaction Inbox"
+        ref={olRef}
+      >
+        <div className={styles.highlight} style={highlightStyles} />
         {transactions.map((transaction, index) => (
           <RovingFocusGroupItem
             key={transaction.id}
@@ -63,8 +93,16 @@ export function Inbox({
               className={styles.item}
               onKeyDown={(e) => handleKeyDown(e, transaction)}
               onClick={(e) => handleClick(e, transaction)}
+              onMouseEnter={() => {
+                setActiveId(transaction.id)
+                liRefs.current[transaction.id]?.focus()
+              }}
               role="option"
               aria-selected={activeId === transaction.id}
+              ref={(li) => {
+                if (li) liRefs.current[transaction.id] = li
+                else delete liRefs.current[transaction.id]
+              }}
             >
               {transaction.name}
             </li>
