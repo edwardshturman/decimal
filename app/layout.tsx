19bb651395b3d0a82519cd7772425745af9587a3
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-09-16 00:16:50 -0700

    Swap fonts to Inter and Roboto Mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Inter, Roboto_Mono } from "next/font/google"
 import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants"
 import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"]
 })
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"]
 })
 
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${inter.variable} ${robotoMono.variable}`}>
       <body>{children}</body>
     </html>
   )
