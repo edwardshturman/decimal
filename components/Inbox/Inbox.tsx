0f10f5cb548fd554ddc7cc4a58e8a68ec260dbae
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-09-16 21:29:08 -0700

    Add chevrons as additional content affordance

--- components/Inbox/Inbox.tsx
+++ components/Inbox/Inbox.tsx
@@ -115,6 +115,18 @@ export function Inbox({ transactions }: { transactions: Transaction[] }) {
 
   return (
     <RovingFocusGroup orientation="vertical">
+      <div
+        className={styles.chevron}
+        style={{ opacity: paginationStart > 0 ? 0.4 : 0 }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M11.4697 9.46967C11.7626 9.17678 12.2374 9.17678 12.5303 9.46967L16.5303 13.4697C16.8232 13.7626 16.8232 14.2374 16.5303 14.5303C16.2374 14.8232 15.7626 14.8232 15.4697 14.5303L12 11.0607L8.53033 14.5303C8.23744 14.8232 7.76256 14.8232 7.46967 14.5303C7.17678 14.2374 7.17678 13.7626 7.46967 13.4697L11.4697 9.46967Z"
+          />
+        </svg>
+      </div>
       <ol
         className={styles.list}
         role="listbox"
@@ -198,6 +210,18 @@ export function Inbox({ transactions }: { transactions: Transaction[] }) {
             </RovingFocusGroupItem>
           ))}
       </ol>
+      <div
+        className={styles.chevron}
+        style={{ opacity: paginationEnd < transactions.length ? 0.4 : 0 }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M11.4697 14.5303C11.7626 14.8232 12.2374 14.8232 12.5303 14.5303L16.5303 10.5303C16.8232 10.2374 16.8232 9.76256 16.5303 9.46967C16.2374 9.17678 15.7626 9.17678 15.4697 9.46967L12 12.9393L8.53033 9.46967C8.23744 9.17678 7.76256 9.17678 7.46967 9.46967C7.17678 9.76256 7.17678 10.2374 7.46967 10.5303L11.4697 14.5303Z"
+          />
+        </svg>
+      </div>
     </RovingFocusGroup>
   )
 }
