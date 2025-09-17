e38e06eea3f36f8712a83c25f7b26d0c4bad4460
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-09-16 20:12:56 -0700

    Add pagination to `Inbox`
    + dynamic opacity to items to indicate proximity to pagination boundaries

--- components/Inbox/Inbox.tsx
+++ components/Inbox/Inbox.tsx
@@ -68,6 +68,30 @@ export function Inbox({ transactions }: { transactions: Transaction[] }) {
     updateHighlight(activeId)
   }, [activeId, updateHighlight])
 
+  const ROW_COUNT = 11
+  const PAGINATE_UP_THRESHOLD = 2
+  const PAGINATE_DOWN_THRESHOLD = 8
+  const [paginationStart, setPaginationStart] = useState(0)
+  const [paginationEnd, setPaginationEnd] = useState(ROW_COUNT)
+
+  function computeOpacity(
+    transactions: Transaction[],
+    index: number,
+    paginationStart: number,
+    paginationEnd: number
+  ) {
+    if (index === ROW_COUNT - 1 && paginationEnd + 1 <= transactions.length)
+      return 0.4
+    if (index === ROW_COUNT - 2 && paginationEnd + 2 <= transactions.length)
+      return 0.6
+    if (index === ROW_COUNT - 3 && paginationEnd + 3 <= transactions.length)
+      return 0.8
+    if (index === 0 && paginationStart - index > 0) return 0.4
+    if (index === 1 && paginationStart - index > 0) return 0.6
+    if (index === 2 && paginationStart - index > 0) return 0.8
+    else return 1
+  }
+
   return (
     <RovingFocusGroup orientation="vertical">
       <ol
@@ -77,49 +101,78 @@ export function Inbox({ transactions }: { transactions: Transaction[] }) {
         ref={olRef}
       >
         <div className={styles.highlight} style={highlightStyles} />
-        {transactions.map((transaction, index) => (
-          <RovingFocusGroupItem
-            key={transaction.id}
-            asChild
-            focusable
-            autoFocus={index === 0}
-            onFocus={() => setActiveId(transaction.id)}
-          >
-            <li
-              className={styles.item}
-              onKeyDown={(e) => handleKeyDown(e, transaction)}
-              onClick={(e) => handleClick(e, transaction)}
-              onMouseEnter={() => {
+        {transactions
+          .sort((a, b) => b.date.getTime() - a.date.getTime())
+          .slice(paginationStart, paginationEnd)
+          .map((transaction, index) => (
+            <RovingFocusGroupItem
+              key={transaction.id}
+              asChild
+              focusable
+              autoFocus={index === 0}
+              onFocus={() => {
                 setActiveId(transaction.id)
-                liRefs.current[transaction.id]?.focus()
-              }}
-              role="option"
-              aria-selected={activeId === transaction.id}
-              ref={(li) => {
-                if (li) liRefs.current[transaction.id] = li
-                else delete liRefs.current[transaction.id]
+
+                // Go down when towards the bottom of the list
+                if (
+                  index === PAGINATE_DOWN_THRESHOLD &&
+                  paginationEnd !== transactions.length
+                ) {
+                  setPaginationStart(paginationStart + 1)
+                  setPaginationEnd(paginationEnd + 1)
+                }
+
+                // Go up when towards the top of the list
+                if (index === PAGINATE_UP_THRESHOLD && paginationStart !== 0) {
+                  setPaginationStart(paginationStart - 1)
+                  setPaginationEnd(paginationEnd - 1)
+                }
               }}
             >
-              <span className={styles.date}>
-                {Intl.DateTimeFormat("en-US", {
-                  month: "2-digit",
-                  day: "2-digit",
-                  year:
-                    transaction.date.getFullYear() !== new Date().getFullYear()
-                      ? "numeric"
-                      : undefined
-                }).format(transaction.date)}
-              </span>
-              <span className={styles.name}>{transaction.name}</span>
-              <span className={styles.amount}>
-                {Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: transaction.currencyCode
-                }).format(transaction.amount)}
-              </span>
-            </li>
-          </RovingFocusGroupItem>
-        ))}
+              <li
+                className={styles.item}
+                onKeyDown={(e) => handleKeyDown(e, transaction)}
+                onClick={(e) => handleClick(e, transaction)}
+                onMouseEnter={() => {
+                  setActiveId(transaction.id)
+                  liRefs.current[transaction.id]?.focus()
+                }}
+                role="option"
+                aria-selected={activeId === transaction.id}
+                ref={(li) => {
+                  if (li) liRefs.current[transaction.id] = li
+                  else delete liRefs.current[transaction.id]
+                }}
+                style={{
+                  opacity: computeOpacity(
+                    transactions,
+                    index,
+                    paginationStart,
+                    paginationEnd
+                  )
+                }}
+              >
+                <span className={styles.date}>
+                  {Intl.DateTimeFormat("en-US", {
+                    month: "2-digit",
+                    day: "2-digit",
+                    year:
+                      transaction.date.getFullYear() !==
+                      new Date().getFullYear()
+                        ? "numeric"
+                        : undefined
+                  }).format(transaction.date)}
+                </span>
+                <span className={styles.name}>{transaction.name}</span>
+                <span className={styles.amount}>
+                  {Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: transaction.currencyCode
+                  }).format(transaction.amount)}
+                </span>
+              </li>
+            </RovingFocusGroupItem>
+          ))}
       </ol>
     </RovingFocusGroup>
   )
